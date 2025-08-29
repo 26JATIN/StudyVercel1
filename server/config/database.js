5737e392df691c644cff16f2f0813c4ef5af7b8e
Jatin Gupta <138003285+26JATIN@users.noreply.github.com>
2025-08-29 21:32:33 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,23 +1,15 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
 
-
-
 exports.Connect=()=>{
 mongoose.connect(process.env.DATABASE_URL)
 .then(()=>{
-    console.log("Db Connect Succesfully");
+    console.log("Db Connect Successfully");
 })
-
 .catch((error)=>{
-    console.log("Failed to connect with db try again");
+    console.log("Failed to connect with db");
     console.error(error);
-    process.exit(1);
+    // Don't exit process in serverless environment
+    // process.exit(1);
 })
-
-
-
-
-
-
 };
